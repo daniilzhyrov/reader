52ebe7b27a6c3ecdca89251daab30b1c9a8990a1
Michael-Shiqy <michael@R-Daneel-Olivaw.localdomain>
2023-04-06 02:06:23 +0200

    test branch 2

--- reader/CreateNewBook.cpp
+++ reader/CreateNewBook.cpp
@@ -17,6 +17,27 @@ using namespace Windows::Storage::Compression;
 
 namespace winrt::reader::implementation
 {
+    /*winrt::Windows::Storage::Streams::IBuffer CompressDataWithXpress(winrt::Windows::Storage::Streams::IBuffer data)
+    {
+        winrt::Windows::Storage::Streams::InMemoryRandomAccessStream inputStream;
+        winrt::Windows::Storage::Streams::InMemoryRandomAccessStream outputStream;
+        auto inputDataWriter = winrt::Windows::Storage::Streams::DataWriter{ inputStream.GetOutputStreamAt(0) };
+
+        inputDataWriter.WriteBuffer(data);
+        inputDataWriter.StoreAsync().get();
+
+        auto compressor = winrt::Windows::Storage::Compression::Compressor{ outputStream, winrt::Windows::Storage::Compression::CompressAlgorithm::Xpress, 0 };
+        auto dataReader = winrt::Windows::Storage::Streams::DataReader{ inputStream.GetInputStreamAt(0) };
+        dataReader.LoadAsync(data.Length()).get();
+
+        auto readBuffer = dataReader.ReadBuffer(data.Length());
+        auto compressedSize = compressor.WriteAsync(readBuffer).get();
+        compressor.FlushAsync().get();
+
+        dataReader = winrt::Windows::Storage::Streams::DataReader{ outputStream.GetInputStreamAt(0) };
+        dataReader.LoadAsync(compressedSize).get();
+        return dataReader.ReadBuffer(compressedSize);
+    }*/
 
     CreateNewBook::CreateNewBook()
     {
@@ -33,8 +54,144 @@ namespace winrt::reader::implementation
         throw hresult_not_implemented();
     }
 
+    //winrt::fire_and_forget CreateNewBook::Click_SelectImages(IInspectable const&, RoutedEventArgs const&)
+    //{
+    //    // Create a FileOpenPicker instance
+    //    Windows::Storage::Pickers::FileOpenPicker filePicker;
+    //    filePicker.ViewMode(Windows::Storage::Pickers::PickerViewMode::Thumbnail);
+    //    filePicker.SuggestedStartLocation(Windows::Storage::Pickers::PickerLocationId::PicturesLibrary);
+
+    //    // Add file type filters for image files
+    //    filePicker.FileTypeFilter().Append(L".jpg");
+    //    filePicker.FileTypeFilter().Append(L".jpeg");
+    //    filePicker.FileTypeFilter().Append(L".png");
+    //    filePicker.FileTypeFilter().Append(L".bmp");
+
+    //    // Set the picker to allow multiple file selection
+    //    filePicker.CommitButtonText(L"Select Images");
+
+    //    // Show the picker and handle the selected files asynchronously
+    //    auto selectedFiles = co_await filePicker.PickMultipleFilesAsync();
+
+    //    if (selectedFiles.Size() > 0)
+    //    {
+    //        // Handle the selected files here
+    //        Windows::Storage::Pickers::FolderPicker folderPicker;
+    //        folderPicker.ViewMode(Windows::Storage::Pickers::PickerViewMode::Thumbnail);
+    //        folderPicker.SuggestedStartLocation(Windows::Storage::Pickers::PickerLocationId::PicturesLibrary);
+    //        //folderPicker.FileTypeFilter().Append(L".cbz");
+
+    //        auto folder = co_await folderPicker.PickSingleFolderAsync();
+
+    //        if (folder != nullptr)
+    //        {
+    //            auto zipFile = co_await folder.CreateFileAsync(L"images.cbz", Windows::Storage::CreationCollisionOption::ReplaceExisting);
+    //            auto zipStream = co_await zipFile.OpenAsync(Windows::Storage::FileAccessMode::ReadWrite);
+
+    //            auto zipOutputStream = zipStream.GetOutputStreamAt(0);
+
+    //            for (const auto& file : selectedFiles)
+    //            {
+    //                // Open the image file for reading
+    //                auto stream = co_await file.OpenReadAsync();
+    //                auto inputStream = stream.GetInputStreamAt(0);
+    //                auto dataReader = Windows::Storage::Streams::DataReader(inputStream);
+
+    //                // Create a compressor for the zip archive
+    //                Windows::Storage::Compression::Compressor compressor(zipOutputStream, Windows::Storage::Compression::CompressAlgorithm::Xpress, 0);
+
+    //                // Read and compress the image data
+    //                co_await dataReader.LoadAsync(stream.Size());
+    //                auto imageData = dataReader.DetachBuffer();
+    //                auto compressedSize = co_await compressor.WriteAsync(imageData);
+
+    //                // Flush the compressor to complete the compression process
+    //                co_await compressor.FlushAsync();
+
+    //                // Print the file name and compressed size
+    //                std::wcout << L"Compressed file: " << file.Name().c_str() << L" - " << compressedSize << L" bytes" << std::endl;
+    //            }
+
+    //            co_await zipOutputStream.FlushAsync();
+    //            zipStream.Close();
+    //        }
+  
+    //    }
+    //}
+
+
     winrt::fire_and_forget CreateNewBook::Click_SelectImages(IInspectable const&, RoutedEventArgs const&)
     {
+//        int err = 0;
+//        const char* temp_folder = "C:\\Users\\shi67\\Pictures\\Temp";
+//#ifdef _WIN32
+//        _putenv_s("TMP", temp_folder);
+//        _putenv_s("TEMP", temp_folder);
+//#else
+//        setenv("TMP", temp_folder, 1);
+//        setenv("TEMP", temp_folder, 1);
+//#endif
+//        std::string temp_archive_path = std::string(temp_folder) + "\\test_archive.cbz";
+//        zip_t* test_archive = zip_open(temp_archive_path.c_str(), ZIP_CREATE | ZIP_EXCL, &err);
+//        if (test_archive == nullptr)
+//        {
+//            winrt::hstring msg = L"test_archive == nullptr";
+//            winrt::Windows::UI::Xaml::Controls::ContentDialog errorDialog;
+//            errorDialog.Title(box_value(L"Error"));
+//            errorDialog.Content(box_value(msg));
+//            errorDialog.CloseButtonText(L"OK");
+//            co_await errorDialog.ShowAsync();
+//            co_return;
+//        }
+//
+//        std::string sample_data = "This is a sample text file.";
+//        std::string file_name = "sample.txt";
+//
+//        zip_source_t* source = zip_source_buffer(test_archive, sample_data.c_str(), sample_data.size(), 0);
+//        if (source == nullptr)
+//        {
+//            winrt::hstring msg = L"Error creating zip source.";
+//            winrt::Windows::UI::Xaml::Controls::ContentDialog errorDialog;
+//            errorDialog.Title(box_value(L"Error"));
+//            errorDialog.Content(box_value(msg));
+//            errorDialog.CloseButtonText(L"OK");
+//            co_await errorDialog.ShowAsync();
+//            co_return;
+//        }
+//
+//        zip_int64_t index = zip_file_add(test_archive, file_name.c_str(), source, ZIP_FL_ENC_UTF_8);
+//        if (index < 0)
+//        {
+//            winrt::hstring msg = L"Error adding file to archive.";
+//            winrt::Windows::UI::Xaml::Controls::ContentDialog errorDialog;
+//            errorDialog.Title(box_value(L"Error"));
+//            errorDialog.Content(box_value(msg));
+//            errorDialog.CloseButtonText(L"OK");
+//            co_await errorDialog.ShowAsync();
+//            co_return;
+//        }
+//
+//        int close_result = zip_close(test_archive);
+//        if (close_result != 0)
+//        {
+//            zip_error_t* error = zip_get_error(test_archive);
+//            const char* error_str = zip_error_strerror(error);
+//
+//            // Convert the error message to a wide string
+//            int requiredSize = MultiByteToWideChar(CP_UTF8, 0, error_str, -1, nullptr, 0);
+//            std::wstring wideErrorMsg(requiredSize, L'\0');
+//            MultiByteToWideChar(CP_UTF8, 0, error_str, -1, &wideErrorMsg[0], requiredSize);
+//
+//            winrt::hstring msg = L"Error closing archive: " + winrt::hstring(wideErrorMsg);
+//            winrt::Windows::UI::Xaml::Controls::ContentDialog errorDialog;
+//            errorDialog.Title(box_value(L"Error"));
+//            errorDialog.Content(box_value(msg));
+//            errorDialog.CloseButtonText(L"OK");
+//            co_await errorDialog.ShowAsync();
+//            co_return;
+//
+//        }
+
         // Create a FileOpenPicker instance
         Windows::Storage::Pickers::FileOpenPicker filePicker;
         filePicker.ViewMode(Windows::Storage::Pickers::PickerViewMode::Thumbnail);
@@ -51,20 +208,47 @@ namespace winrt::reader::implementation
 
         // Show the picker and handle the selected files asynchronously
         auto selectedFiles = co_await filePicker.PickMultipleFilesAsync();
+
         if (selectedFiles.Size() > 0)
         {
+
             // Handle the selected files here
             Windows::Storage::Pickers::FolderPicker folderPicker;
             folderPicker.ViewMode(Windows::Storage::Pickers::PickerViewMode::Thumbnail);
             folderPicker.SuggestedStartLocation(Windows::Storage::Pickers::PickerLocationId::PicturesLibrary);
-            //folderPicker.FileTypeFilter().Append(L".cbz");
 
             auto folder = co_await folderPicker.PickSingleFolderAsync();
+
             if (folder != nullptr)
             {
-                auto zipFile = co_await folder.CreateFileAsync(L"images.cbz", Windows::Storage::CreationCollisionOption::ReplaceExisting);
-                auto zipStream = co_await zipFile.OpenAsync(Windows::Storage::FileAccessMode::ReadWrite);
-                auto zipOutputStream = zipStream.GetOutputStreamAt(0);
+                std::wstring zipFilePath = folder.Path().c_str() + std::wstring(L"\\images.cbz");
+
+                // Convert wide string to narrow string using Windows functions
+                int requiredSize = WideCharToMultiByte(CP_UTF8, 0, zipFilePath.c_str(), -1, nullptr, 0, nullptr, nullptr);
+                std::string narrowZipFilePath(requiredSize, '\0');
+                WideCharToMultiByte(CP_UTF8, 0, zipFilePath.c_str(), -1, &narrowZipFilePath[0], requiredSize, nullptr, nullptr);
+
+#ifdef _WIN32
+                _putenv_s("TMP", narrowZipFilePath.c_str());
+                _putenv_s("TEMP", narrowZipFilePath.c_str());
+#else
+                setenv("TMP", narrowZipFilePath.c_str(), 1);
+                setenv("TEMP", narrowZipFilePath.c_str(), 1);
+#endif
+
+                int error = 0;
+                zip_t* archive = zip_open(narrowZipFilePath.c_str(), ZIP_CREATE | ZIP_EXCL, &error);
+
+                if (!archive)
+                {
+                    winrt::hstring msg = L"Error opening archive. Error code: " + winrt::to_hstring(error);
+                    winrt::Windows::UI::Xaml::Controls::ContentDialog errorDialog;
+                    errorDialog.Title(box_value(L"Error"));
+                    errorDialog.Content(box_value(msg));
+                    errorDialog.CloseButtonText(L"OK");
+                    co_await errorDialog.ShowAsync();
+                    co_return;
+                }
 
                 for (const auto& file : selectedFiles)
                 {
@@ -73,30 +257,73 @@ namespace winrt::reader::implementation
                     auto inputStream = stream.GetInputStreamAt(0);
                     auto dataReader = Windows::Storage::Streams::DataReader(inputStream);
 
-                    // Create a compressor for the zip archive
-                    Windows::Storage::Compression::Compressor compressor(zipOutputStream, Windows::Storage::Compression::CompressAlgorithm::Lzms, 0);
-
-                    // Read and compress the image data
+                    // Read the image data
                     co_await dataReader.LoadAsync(stream.Size());
                     auto imageData = dataReader.DetachBuffer();
-                    auto compressedSize = co_await compressor.WriteAsync(imageData);
+                    std::vector<uint8_t> buffer(imageData.Length());
+                    auto dataReader2 = Windows::Storage::Streams::DataReader::FromBuffer(imageData);
+                    dataReader2.ReadBytes(buffer);
 
-                    // Flush the compressor to complete the compression process
-                    co_await compressor.FlushAsync();
+                    // Convert wide string to narrow string using Windows functions
+                    int requiredSize = WideCharToMultiByte(CP_UTF8, 0, file.Name().c_str(), -1, nullptr, 0, nullptr, nullptr);
+                    std::string narrowFileName(requiredSize, '\0');
+                    WideCharToMultiByte(CP_UTF8, 0, file.Name().c_str(), -1, &narrowFileName[0], requiredSize, nullptr, nullptr);
 
-                    // Print the file name and compressed size
-                    std::wcout << L"Compressed file: " << file.Name().c_str() << L" - " << compressedSize << L" bytes" << std::endl;
+                    // Add a new entry to the zip archive
+                    zip_source_t* source = zip_source_buffer(archive, buffer.data(), buffer.size(), 0);
+                    if (source == nullptr)
+                    {
+                        winrt::hstring msg = L"Error creating zip source for file: " + file.Name();
+                        winrt::Windows::UI::Xaml::Controls::ContentDialog errorDialog;
+                        errorDialog.Title(box_value(L"Error"));
+                        errorDialog.Content(box_value(msg));
+                        errorDialog.CloseButtonText(L"OK");
+                        co_await errorDialog.ShowAsync();
+                        zip_close(archive);
+                        co_return;
+                    }
+
+                    zip_int64_t index = zip_file_add(archive, narrowFileName.c_str(), source, ZIP_FL_ENC_UTF_8);
+                    if (index < 0)
+                    {
+                        winrt::hstring msg = L"Error adding file to archive: " + file.Name();
+                        winrt::Windows::UI::Xaml::Controls::ContentDialog errorDialog;
+                        errorDialog.Title(box_value(L"Error"));
+                        errorDialog.Content(box_value(msg));
+                        errorDialog.CloseButtonText(L"OK");
+                        co_await errorDialog.ShowAsync();
+                        co_return;
+                    }
                 }
 
-                co_await zipOutputStream.FlushAsync();
-                zipStream.Close();
+                if (archive == nullptr)
+                {
+                    winrt::hstring msg = L"Archive not valid";
+                    winrt::Windows::UI::Xaml::Controls::ContentDialog errorDialog;
+                    errorDialog.Title(box_value(L"Error"));
+                    errorDialog.Content(box_value(msg));
+                    errorDialog.CloseButtonText(L"OK");
+                    co_await errorDialog.ShowAsync();
+                    co_return;
+                }
+
+                zip_close(archive);
+                /*if (zip_close(archive) == -1)
+                {
+                    const char* error_str = zip_strerror(archive);
+                    winrt::hstring errorMsg = winrt::to_hstring(error_str);
+                    winrt::hstring msg = L"Error closing archive: " + errorMsg;
+                    winrt::Windows::UI::Xaml::Controls::ContentDialog errorDialog;
+                    errorDialog.Title(box_value(L"Error"));
+                    errorDialog.Content(box_value(msg));
+                    errorDialog.CloseButtonText(L"OK");
+                    co_await errorDialog.ShowAsync();
+                    co_return;
+                }*/
             }
         }
     }
 
-    void CreateNewBook::Click_Save(IInspectable const&, RoutedEventArgs const&)
-    {
-        // Handle the "Select Images" button click event here
-    }
+   
 }
 
--- reader/CreateNewBook.h
+++ reader/CreateNewBook.h
@@ -1,16 +1,23 @@
 ﻿#pragma once
 
 #include "CreateNewBook.g.h"
+#include <Windows.h>
 #include <fstream>
 #include <iostream>
 #include <vector>
 #include <filesystem>
+#include <AclAPI.h>
 #include <winrt/Windows.Storage.h>
 #include <winrt/windows.storage.streams.h>
+#include <winrt/Windows.Storage.Streams.h>
 #include <winrt/Windows.Storage.Pickers.h>
 #include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.Storage.Compression.h>
 #include "winrt/Windows.UI.Core.h"
+#include <winrt/Windows.Foundation.h>
+#include <zip.h>
+
+
 
 namespace winrt::reader::implementation
 {
@@ -23,7 +30,7 @@ namespace winrt::reader::implementation
 
         //void Click_SelectImages(Windows::Foundation::IInspectable const&, Windows::UI::Xaml::RoutedEventArgs const&);
         winrt::fire_and_forget Click_SelectImages(Windows::Foundation::IInspectable const&, Windows::UI::Xaml::RoutedEventArgs const&);
-        void Click_Save(Windows::Foundation::IInspectable const&, Windows::UI::Xaml::RoutedEventArgs const&);
+        //void Click_Save(Windows::Foundation::IInspectable const&, Windows::UI::Xaml::RoutedEventArgs const&);
     };
 }
 
